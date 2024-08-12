0a9b00e6c7079a4ccb5303525798f228057c5e9e
Ranjit Rupnawar <162142856+ranjitrupnawar@users.noreply.github.com>
2024-08-12 20:00:01 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+// test.js
+const assert = require('assert');
+
+// A simple test case
+assert.strictEqual("Hello World", "Hello World", "The output should be 'Hello World'");
+console.log("All tests passed!");
